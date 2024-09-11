b1dd1117bcf0737a84bfdb2665a1399ab7c5650f
shwethprojectinfo <82966shwe@gmail.com>
2024-09-11 17:31:26 +0530

    commit

--- AutomationDemo/src/main/java/FlipkartPages/LoginPage.java
+++ AutomationDemo/src/main/java/FlipkartPages/LoginPage.java
@@ -1,14 +1,25 @@
 package FlipkartPages;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
 	
+	
+
+	WebDriver driver = null;
+	
+
 	@FindBy(xpath="//a//span[contains(text(),'Login')]")
 	private WebElement loginButton;
 	
 	@FindBy(xpath="//label//span[contains(text(),'Enter Email/Mobile number')]")
 	private WebElement emailTextField;
-
+	
+	@FindBy(xpath = "//button[@class='QqFHMw twnTnD _7Pd1Fp']")
+	private WebElement login;
+	
+	
 }
